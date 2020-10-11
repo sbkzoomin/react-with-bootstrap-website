a1f0c46c2aaf1cd976c57bc5ad6598e66a0eb157
sbkzoomin <bharathsriram5@gmail.com>
2020-10-12 01:16:20 +0530

    1 binaries uploaded

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -3,7 +3,16 @@ import React from 'react';
 class LandingPage extends React.Component {
   render(){
     return (
-      <h1>LandingPage</h1>
+      <React.Fragment>
+        <div className="landingPage">
+          <div className="wrapper">
+            <div className="div">
+              <h3 className="dispaly-3">Welcome to Venu's Fast Food</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam scelerisque magna dui, ac laoreet massa tempus vel.</p>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
